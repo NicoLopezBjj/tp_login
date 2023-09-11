d9873423b5d6c41bcfcb732ebc132947b5a56997
Nico <lopeznnr@gmail.com>
2023-09-11 11:27:57 -0300

    var bodyParser

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require ('express')
 const mysql = require ('mysql')
 const path= require ('path')
 const session = require ('express-session')
+const bodyParser = require ('body-parser')
 require('dotenv').config()
 
 
